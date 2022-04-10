d341fe509e9ee70bca8a3ec6ee1eb4ee51d95e5c
lloydvue <lloydvue94@gmail.com>
2022-04-10 15:20:30 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName ='bob'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return setBestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+    const changeLeastFavoriteCustomer = 'Alvee'
+    changeLeastFavoriteCustomer = 'Damian'
+}
